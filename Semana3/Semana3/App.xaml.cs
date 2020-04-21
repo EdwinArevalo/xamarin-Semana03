07189130883ed17816b4cad4127f2c5f5afe06f4
EdwinWilliamArevalo <edwin.arevalo@tecsup.edu.pe>
2020-04-20 21:48:56 -0500

    first commit

---
+++ Semana3/Semana3/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+namespace Semana3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new StackLayoutCode();
+            //MainPage = new AbsoluteLayoutExplorationCode();
+            //MainPage = new GridDemo();
+            //MainPage = new RelativeLayoutDemo();
+            MainPage = new ScrollViewDemo();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
